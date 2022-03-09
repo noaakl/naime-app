84f7a4de082e6d95043d097e8e48d3608a5ab624
noaakl <62835962+noaakl@users.noreply.github.com>
2022-03-09 19:47:29 +0200

    filter with delay

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,22 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Styles from '../App.module.scss'
 import SearchCount from './SearchCount'
 import RankInfo from './RankInfo'
-import {Card} from 'react-bootstrap'
-import {Row} from 'react-bootstrap'
-import {Col} from 'react-bootstrap'
-import { Dropdown } from "react-bootstrap";
+import { Card, Row, Col, Dropdown } from 'react-bootstrap'
+import Form from 'react-bootstrap/Form'
 import { SortDown, FunnelFill } from 'react-bootstrap-icons';
 
 const Results = ({ searchedName, algorithemsData }) => {
     const suggestionsExist = typeof algorithemsData.soundex !== 'undefined'
+    const algorithemsNames = Object.keys(algorithemsData).slice()
     // const [likedRanks, setLikedRanks] = useState([])
     // const [dislikedRanks, setDislikedRanks] = useState([])
     const [sortValue, setSortValue] = useState("Default A-Z")
     const [isAZData, setAZData] = useState(true)
 
+    const [algorithems, setAlgorithems] = useState(algorithemsNames)
+
+    useEffect(() => {
+        setAlgorithems(algorithemsNames)
+    }, [suggestionsExist]);
+
     const sortFunc = (a, b) => {
         return isAZData ? a.candidate.localeCompare(b.candidate) : b.user_rank - a.user_rank
+    }
+
+    const handleFilterCheck = (value) => {
+        const checkedAlgorithems = algorithems.slice()
+        if (checkedAlgorithems.includes(value)) {
+            const valueIndex = checkedAlgorithems.indexOf(value);
+            checkedAlgorithems.splice(valueIndex, 1);
+        }
+        else {
+            checkedAlgorithems.push(value)
+        }
+        setAlgorithems(checkedAlgorithems)
+    }
+
+    const isChecked = (value) => {
+        return algorithems.includes(value)
     }
 
     Object.keys(algorithemsData).forEach(algorithem => { algorithemsData[algorithem].sort(sortFunc) });
@@ -25,96 +46,117 @@ const Results = ({ searchedName, algorithemsData }) => {
         <div className={Styles.result_wrapper}>
             <div className={Styles.result_wrapper}>
                 <Row>
-                <h2 className={Styles.result_title}>Suggested Synonyms for the name '{searchedName}'</h2>
+                    <h2 className={Styles.result_title}>Suggested Synonyms for the name '{searchedName}'</h2>
                 </Row>
                 <Row>
-                <Col style={{marginTop:-20, marginBottom:30}}>
-                <strong>more info</strong><SearchCount searchedName={searchedName} />
-                </Col>
+                    <Col style={{ marginTop: -20, marginBottom: 30 }}>
+                        <strong>more info</strong><SearchCount searchedName={searchedName} />
+                    </Col>
                 </Row>
                 <Row xs={1} md={1} lg={4}>
-                    <Col style={{margin:5}}>
-                <Dropdown>
-                    <strong>sort by  </strong><SortDown style={{marginRight:"15px", marginLeft:"5px"}}/><Dropdown.Toggle className={Styles.sort} variant="secondary" size="sm" id="dropdown-basic">
-                        {sortValue}
-                    
-                    </Dropdown.Toggle>
+                    <Col style={{ margin: 5 }}>
+                        <Dropdown>
+                            <strong>sort by  </strong><SortDown style={{ marginRight: "15px", marginLeft: "5px" }} /><Dropdown.Toggle className={Styles.sort} variant="secondary" size="sm" id="dropdown-basic">
+                                {sortValue}
 
-                    <Dropdown.Menu>
-                        <Dropdown.Item
-                            onClick={() => {
-                                setAZData(false)
-                                setSortValue("User Rank")
-                            }}>
-                            User Rank</Dropdown.Item>
-                        <Dropdown.Item onClick={() => {
-                            setAZData(true)
-                            setSortValue("Default A-Z")
-                        }}>Default A-Z</Dropdown.Item>
-                        {/* <Dropdown.Item onClick={()=>{}}>User Rank</Dropdown.Item> */}
-                    </Dropdown.Menu>
-                </Dropdown>
-                </Col>
+                            </Dropdown.Toggle>
 
-                {/* <Col style={{marginTop:5, marginLeft:-270}}> */}
-                <Col style={{margin:5}}>
-                <Dropdown>
-                   <strong>filter by  </strong><FunnelFill style={{ marginRight:"15px", marginLeft:"5px" }}/>
-                   <Dropdown.Toggle className={Styles.sort} variant="secondary" size="sm" id="dropdown-basic">
-                       {sortValue}
+                            <Dropdown.Menu>
+                                <Dropdown.Item
+                                    onClick={() => {
+                                        setAZData(false)
+                                        setSortValue("User Rank")
+                                    }}>
+                                    User Rank</Dropdown.Item>
+                                <Dropdown.Item onClick={() => {
+                                    setAZData(true)
+                                    setSortValue("Default A-Z")
+                                }}>Default A-Z</Dropdown.Item>
+                                {/* <Dropdown.Item onClick={()=>{}}>User Rank</Dropdown.Item> */}
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </Col>
 
-                   </Dropdown.Toggle>
+                    {/* <Col style={{marginTop:5, marginLeft:-270}}> */}
+                    <Col style={{ margin: 5 }}>
+                        <Dropdown>
+                            <strong>filter by  </strong><FunnelFill style={{ marginRight: "15px", marginLeft: "5px" }} />
+                            <Dropdown.Toggle variant="secondary" size="sm" id="dropdown-basic">
+                                {sortValue}
 
-                   <Dropdown.Menu>
-                       <Dropdown.Item bsPrefix="input"
-                           onClick={() => {
-                               setAZData(false)
-                               setSortValue("User Rank")
-                           }}>
-                           User Rank</Dropdown.Item>
-                       <Dropdown.Item type="radio" onClick={() => {
-                           setAZData(true)
-                           setSortValue("Default A-Z")
-                       }}>Default A-Z</Dropdown.Item>
-                   </Dropdown.Menu>
-                </Dropdown>
-                </Col>
+                            </Dropdown.Toggle>
 
-                <Col style={{margin:5}}>
-                <strong>more info</strong><SearchCount searchedName={searchedName} />
-                </Col>
+                            <Dropdown.Menu>
+                                {Object.keys(algorithemsData).map((algorithem) => {
+                                    return (
+                                        <Dropdown.Item className={Styles.filter} key={algorithem}>
+                                            {/* <Form.Check type="checkbox" id={algorithem}>
+                                                <Form.Check.Input type="checkbox" isValid onChange={() => handleFilterCheck(algorithem)}/>
+                                                <Form.Check.Label>{algorithem}</Form.Check.Label>
+                                            </Form.Check> */}
+
+
+                                            <input
+                                            key={algorithem}
+                                            className={Styles.filter}
+                                            label={algorithem}
+                                            name="controlled"
+                                            type="checkbox"
+                                            id={algorithem}
+                                            checked={isChecked(algorithem)}
+                                            onChange={() => handleFilterCheck(algorithem)}
+                                        /><label>{` ${algorithem}`}</label>
+
+
+
+                                            {/* <Form.Check className={Styles.filter} type="checkbox" id={`check-api-checkbox`} value="option1" checked={true}> */}
+                                            {/* User Rank */}
+                                            {/* </Form.Check> */}
+                                        </Dropdown.Item>
+                                    )
+                                })
+                                }
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </Col>
+
+                    <Col style={{ margin: 5 }}>
+                        <strong>more info</strong><SearchCount searchedName={searchedName} />
+                    </Col>
                 </Row>
                 {/* </div> */}
             </div>
-            <Row lg={4} md={3} sm={2} xs={1} className="g-4" style={{margin:0, padding:0}}>
-            {Object.keys(algorithemsData).map((algorithem) => {
-                return (
-                    <Col key={algorithem} style={{margin:0, display: "flex",
-                        flexWrap: "wrap", padding:0}}>
-                    <Card id={algorithem} className="shadow-sm p-3 mb-3 bg-white rounded"
-                    style={{height:'380px', width:"95%", margin:5, padding:0}}
-                    >
-                        <Card.Body style={{margin:0, padding:0}}>
-                            <h3 style={{textAlign:"center"}}>{algorithem}</h3>
-                        {algorithemsData[algorithem].map((name) => {
-                            // const showLikeRank = algorithem in algorithems && name?.add_rank !== -1
-                            // const showDislikeRank = algorithem in algorithems && name?.add_rank !== 1
-                            return (
-                                <>
-                                <Row>
-                                 <Col className={Styles.resultcol}>
-                                <div key={`${algorithem}_${name.candidate}`} className={Styles.result}>{name.candidate}
-                                </div>
-                                </Col>
-                                <Col className={Styles.resultcolrank}>
-                                 <RankInfo searchedName={searchedName} name={name} algorithem={algorithem}/>
-                                 </Col>
-                                </Row>
-                                </>
-                            )
-                        })}</Card.Body>
-                    </Card></Col>)
-            })}
+            <Row lg={4} md={3} sm={2} xs={1} className="g-4" style={{ margin: 0, padding: 0 }}>
+                {algorithems.map((algorithem) => {
+                    return (
+                        <Col key={algorithem} style={{
+                            margin: 0, display: "flex",
+                            flexWrap: "wrap", padding: 0
+                        }}>
+                            <Card id={algorithem} className="shadow-sm p-3 mb-3 bg-white rounded"
+                                style={{ height: '380px', width: "95%", margin: 5, padding: 0 }}
+                            >
+                                <Card.Body style={{ margin: 0, padding: 0 }}>
+                                    <h3 style={{ textAlign: "center" }}>{algorithem}</h3>
+                                    {algorithemsData[algorithem].map((name) => {
+                                        // const showLikeRank = algorithem in algorithems && name?.add_rank !== -1
+                                        // const showDislikeRank = algorithem in algorithems && name?.add_rank !== 1
+                                        return (
+                                            <>
+                                                <Row>
+                                                    <Col className={Styles.resultcol}>
+                                                        <div key={`${algorithem}_${name.candidate}`} className={Styles.result}>{name.candidate}
+                                                        </div>
+                                                    </Col>
+                                                    <Col className={Styles.resultcolrank}>
+                                                        <RankInfo searchedName={searchedName} name={name} algorithem={algorithem} />
+                                                    </Col>
+                                                </Row>
+                                            </>
+                                        )
+                                    })}</Card.Body>
+                            </Card></Col>)
+                })}
             </Row>
         </div>
         : <div className={Styles.result_wrapper}>
