bcc00359e3cbb2a287310dfe7a6985a783b4c007
noaakl <62835962+noaakl@users.noreply.github.com>
2022-04-21 14:25:03 +0300

    local storage works

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from 'react-redux'
-import store from './store/store'
+import {store, persistor} from './store/store'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import UserInfo from "./components/UserInfo";
 import SingUp from "./components/SignUp";
-// import { useState } from 'react';
 import Login from "./components/Login";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from "./components/NavBar";
-
-
+import { PersistGate } from 'redux-persist/integration/react'
 
 export default function App() {
-  // const [showAbout, setShowAbout] = useState(false);
-  // const handleAbout = () => setShowAbout(!showAbout);
   return (
     <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
     <BrowserRouter>
     <NavBar/>
-    
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/userInfo" element={<UserInfo />}></Route>
@@ -28,6 +24,7 @@ export default function App() {
         <Route path="/login" element={<Login />}></Route>
       </Routes>
     </BrowserRouter>
+    </PersistGate>
     </Provider>
   );
 }
