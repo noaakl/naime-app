91070239c81d647f27a0bb32fc9c0f7dfe40319b
noaakl <62835962+noaakl@users.noreply.github.com>
2022-06-04 14:31:45 +0300

    separate functions

--- src/components/Home.js
+++ src/components/Home.js
@@ -22,8 +22,6 @@ const Home = () => {
     const suggestionsExist = algorithmsData.length > 0
     const { name } = useParams();
     const navigate = useNavigate();
-    // const navigate = useNavigate();
-    // const handleOnClick = useCallback((name) => navigate(`/search/${name}`, {replace: true}), [navigate]);
 
     useEffect(() => {
         setSuggestions([])
@@ -35,7 +33,7 @@ const Home = () => {
         setShowResults(false)
         if (name) {
             setNameValue(name)
-            searchName(name)
+            handleSearch(name)
         }
     }, [username, name]);
 
@@ -64,87 +62,87 @@ const Home = () => {
         );
     };
 
-    const searchName = (nameValue) => {
+    const handleSearch = (nameValue) => {
         if (nameValue !== '') {
-
-            let searchVal = ""
-            searchVal = nameValue
+            const searchVal = nameValue
             const doSearch = searchVal !== '' && isValidSearchVal(searchVal)
             setShowResults(doSearch)
             if (doSearch) {
-                // handleOnClick()
                 navigate(`/search/${searchVal}`);
-                setNameToSearch(nameValue)
-                setAlgorithmsData({})
-                setRanks({})
-                const searchData = {
-                    "name": searchVal,
-                    "username": username
-                }
-                axios({
-                    method: "GET",
-                    url: `/api/suggestions`,
-                    params: searchData
-                })
-                    .then((response) => {
-                        let addedSuggestions = []
-                        let addedSearchedNames = []
-                        let addedAlgorithmsData = []
-                        response.data.forEach(suggestionsData => {
-                            if (typeof suggestionsData.soundex !== 'undefined') {
-                                addedSuggestions = [...addedSuggestions, suggestionsData];
-                                const spoken_name_2_vec = suggestionsData.spoken_name_2_vec;
-                                const double_metaphone = suggestionsData.double_metaphone;
-                                const family_trees = suggestionsData.family_trees;
-                                const match_rating_codex = suggestionsData.match_rating_codex;
-                                const metaphone = suggestionsData.metaphone;
-                                const nysiis = suggestionsData.nysiis;
-                                const soundex = suggestionsData.soundex;
-
-                                // setSearchedNames([...oldSearchedNames, suggestionsData.name]);
-                                addedSearchedNames = [...addedSearchedNames, suggestionsData.name];
-                                addedAlgorithmsData = [...addedAlgorithmsData, {
-                                    'SpokenName2Vec': spoken_name_2_vec,
-                                    'Double Metaphone': double_metaphone,
-                                    'GRAFT': family_trees,
-                                    'Match Rating Codex': match_rating_codex,
-                                    'Metaphone': metaphone,
-                                    'Nysiis': nysiis,
-                                    'Soundex': soundex
-                                }];
-                            }
-                            else {
-                                addedAlgorithmsData = [...addedAlgorithmsData, {}]
-                            }
-
-                        });
-                        setSuggestions(addedSuggestions);
-                        setSearchedNames(addedSearchedNames);
-                        setAlgorithmsData(addedAlgorithmsData);
-
-                        axios({
-                            method: "GET",
-                            url: `/api/rankCount`,
-                            params: { "name": searchVal }
-                        })
-                            .then((response) => {
-                                let addedRanks = []
-                                response.data.forEach(ranksData => {
-                                    if (typeof ranksData.likes !== 'undefined') {
-                                        const likes = ranksData.likes
-                                        const dislikes = ranksData.dislikes
-                                        addedRanks = [...addedRanks, {
-                                            'likes': likes,
-                                            'dislikes': dislikes
-                                        }];
-                                    }
-                                })
-                                setRanks(addedRanks);
-                            })
-                    })
-
+                searchName(nameValue)
             }
         }
+    }
+
+    const searchName = (searchVal) => {
+        setNameToSearch(searchVal)
+        setAlgorithmsData({})
+        setRanks({})
+        const searchData = {
+            "name": searchVal,
+            "username": username
+        }
+        axios({
+            method: "GET",
+            url: `/api/suggestions`,
+            params: searchData
+        })
+            .then((response) => {
+                let addedSuggestions = []
+                let addedSearchedNames = []
+                let addedAlgorithmsData = []
+                response.data.forEach(suggestionsData => {
+                    if (typeof suggestionsData.soundex !== 'undefined') {
+                        addedSuggestions = [...addedSuggestions, suggestionsData];
+                        const spoken_name_2_vec = suggestionsData.spoken_name_2_vec;
+                        const double_metaphone = suggestionsData.double_metaphone;
+                        const family_trees = suggestionsData.family_trees;
+                        const match_rating_codex = suggestionsData.match_rating_codex;
+                        const metaphone = suggestionsData.metaphone;
+                        const nysiis = suggestionsData.nysiis;
+                        const soundex = suggestionsData.soundex;
+
+                        // setSearchedNames([...oldSearchedNames, suggestionsData.name]);
+                        addedSearchedNames = [...addedSearchedNames, suggestionsData.name];
+                        addedAlgorithmsData = [...addedAlgorithmsData, {
+                            'SpokenName2Vec': spoken_name_2_vec,
+                            'Double Metaphone': double_metaphone,
+                            'GRAFT': family_trees,
+                            'Match Rating Codex': match_rating_codex,
+                            'Metaphone': metaphone,
+                            'Nysiis': nysiis,
+                            'Soundex': soundex
+                        }];
+                    }
+                    else {
+                        addedAlgorithmsData = [...addedAlgorithmsData, {}]
+                    }
+
+                });
+                setSuggestions(addedSuggestions);
+                setSearchedNames(addedSearchedNames);
+                setAlgorithmsData(addedAlgorithmsData);
+
+                axios({
+                    method: "GET",
+                    url: `/api/rankCount`,
+                    params: { "name": searchVal }
+                })
+                    .then((response) => {
+                        let addedRanks = []
+                        response.data.forEach(ranksData => {
+                            if (typeof ranksData.likes !== 'undefined') {
+                                const likes = ranksData.likes
+                                const dislikes = ranksData.dislikes
+                                addedRanks = [...addedRanks, {
+                                    'likes': likes,
+                                    'dislikes': dislikes
+                                }];
+                            }
+                        })
+                        setRanks(addedRanks);
+                    })
+            })
     }
 
     return (
@@ -155,10 +153,10 @@ const Home = () => {
                     <div style={{ textAlign: "center" }}>Similar Name Suggestor</div>
                     <div className={Styles.search_wraper} >
                         <div className={Styles.search_inner}>
-                            <input className={Styles.form_control} value={nameValue} onKeyPress={(e) => e.key === "Enter" ? searchName(nameValue) : ""}
+                            <input className={Styles.form_control} value={nameValue} onKeyPress={(e) => e.key === "Enter" ? handleSearch(nameValue) : ""}
                                 onChange={(e) => setNameValue(e.target.value)} placeholder="Write name here (i.e Mike, Christina)" />
                             <span className={Styles.search_btn}>
-                                <button className={Styles.search_btn} id="searchName" onClick={() => { searchName(nameValue) }} method="POST" >
+                                <button className={Styles.search_btn} id="searchName" onClick={() => { handleSearch(nameValue) }} method="POST" >
                                     <Search size={20} />
                                 </button>
                             </span>
