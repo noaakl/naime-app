3a3c420a48ec11313033248dd33d5ee6234f63ca
Guy Shimony <shimonyguy@gmail.com>
2022-06-08 19:40:17 +0300

    add algorithms description

--- src/components/Info.js
+++ src/components/Info.js
@@ -23,9 +23,8 @@ const Info = () => {
                                 justifyContent: "center"
                             }}><Card.Body className={Styles.card_body_about}>
                                     <h5 style={{ textAlign: "center" }}>SpokenName2Vec</h5>
-                                    a novel and generic deep learning algorithm that utilizes automated speech in most given languages and accents for various name-related tasks.
-                                    SpokenName2Vec algorithm generates a fixed-length vector representation derived from an audio segment that expresses the way people pronounce a name in a given language and accent.
-                                    This model is much more sophisticated than other algorithms proposed for the task of synonym suggestion, and its ability to detect synonyms that sound alike but are written differently is notable.
+                                        A novel and generic deep learning algorithm that utilizes automated speech in most given languages and accents for various name-related tasks.
+                                        The SpokenName2Vec algorithm encodes a text into a simple plaintext code and generates a fixed-length vector representation derived from an audio segment that expresses the way people pronounce a name in a given language and accent. This model is much more sophisticated than other algorithms proposed for the task of synonym suggestion, and its ability to detect synonyms that sound alike but are written differently is notable.
                                     <Card.Img variant="bottom" src={SpokenName2Vec} className={Styles.card_img}/>
                                 </Card.Body></Card>
                         </Row>
@@ -37,14 +36,13 @@ const Info = () => {
                                 justifyContent: "center"
                             }}><Card.Body className={Styles.card_body_about}>
                                     <h5 style={{ textAlign: "center" }}>GRAFT</h5>
-                                    a novel algorithm for improving the suggestion of alternative spellings associated with a name provided as a query.
-                                    The algorithm based on the construction and analysis of digitized family trees, combined with network science.
-                                    By constructing digitized family trees, the algorithm utilize the valuable ancestral information in these family trees to detect family members who share a similar name.
-                                    Afterward, by connecting names that many family members have preserved over generations, we construct a graph based on names that reflects the evolution of names over generations.
-                                    Then, we search for the name provided as a query in the graph and select candidates to be suggested as alternative names according to a general ordering function.
+                                        A novel algorithm for improving the suggestion of alternative spellings associated with a name provided as a query.
+                                        The algorithm is based on the construction and analysis of digitized family trees, combined with network science.
+                                        By constructing digitized family trees, the algorithm utilizes the valuable ancestral information in these family trees to detect family members who share a similar name. A graph is constructed by connecting names that many family members have preserved over generations. This graph is based on names that reflect the evolution of names over generations.
+                                        The name provided as a query is searched in the graph and candidates are suggested as alternative names according to a general ordering function consisting of the network’s structure, the string and the phonetic similarity between the name provided as a query and the candidate.
                                     <br></br><br></br>
                                     <Card.Img variant="bottom" src={GRAFT} className={Styles.card_img}/>
-                                    <strong>for more information - click here for the artical </strong>
+                                    <strong>for more information - click here for the article </strong>
                                     </Card.Body></Card>
                         </Row>
                     </Col>
