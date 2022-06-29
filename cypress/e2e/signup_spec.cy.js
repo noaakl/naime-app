446cf6002813b92db84e7a91e0f1e86f3e82918c
noaakl <62835962+noaakl@users.noreply.github.com>
2022-06-29 22:21:24 +0300

    cypress

--- cypress/e2e/signup_spec.cy.js
+++ cypress/e2e/signup_spec.cy.js
@@ -14,7 +14,7 @@ describe('Signup', () => {
       cy.contains('Please enter a valid E-Mail Address, e.g user@naime.com').should('be.visible');
       cy.contains('Password must contain at least 6 characters').should('be.visible');
       cy.contains("Confirmation Password does't match the password. Please try again").should('be.visible');
-      cy.get('#formUserName').click().type("sername1")
+      cy.get('#formUserName').click().type("sername")
       cy.get('form > .btn').click()
       cy.contains("User Name already exist. Please try again").should('be.visible');
     })
@@ -22,14 +22,14 @@ describe('Signup', () => {
     it('passes', () => {
       cy.visit('http://localhost:3000/signUp');
       cy.get('.navbar').should('be.visible')
-      cy.get('#formUserName').click().type("username")
+      cy.get('#formUserName').click().type("username1")
       cy.get('#formFirstName').click().type("firstname")
       cy.get('#formLastName').click().type("lastname")
       cy.get('#formEmail').click().type("username@gmail.com")
       cy.get('#formBasicPassword').click().type("21hdj3u22614iuoh")
       cy.get('#formConfirmationPassword').click().type("21hdj3u22614iuoh")
       cy.get('form > .btn').click()
-      cy.contains('User username successfully registered').should('be.visible');
+      cy.contains('User username1 successfully registered').should('be.visible');
       cy.get('.modal-footer > .btn').click()
       cy.url().should('include', '/login');
     })
